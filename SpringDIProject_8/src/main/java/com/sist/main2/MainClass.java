ee8330efb6ea9166adce0dd7425b33ee40668aa7
ajskd5 <ajskd5@naver.com>
2022-08-24 15:02:31 +0900

    DI (MyBatis)

---
+++ SpringDIProject_8/src/main/java/com/sist/main2/MainClass.java
@@ -0,0 +1,68 @@
+package com.sist.main2;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.*;
+
+public class MainClass {
+	public static void main(String[] args) {
+		ApplicationContext app = new AnnotationConfigApplicationContext("app1.xml");
+		MovieDAO dao = (MovieDAO)app.getBean("dao");
+
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("======== Menu ========");
+		System.out.println("1. 제목");
+		System.out.println("2. 장르");
+		System.out.println("3. 출연");
+		System.out.println("4. 제목 + 장르");
+		System.out.println("5. 제목 + 출연");
+		System.out.println("6. 출연 + 장르");
+		System.out.println("7. 제목 + 장르 + 출연");
+		System.out.println("======================");
+		System.out.print("메뉴 선택 : ");
+		int menu = scan.nextInt();
+		
+		Map map = new HashMap();
+		List<MovieVO> list = new ArrayList<MovieVO>();
+		ArrayList<String> nList = new ArrayList<String>();
+		
+		if(menu == 1) {
+			nList.add("T");
+		} else if(menu == 2) {
+			nList.add("G");
+		} else if(menu == 3) {
+			nList.add("A");
+		} else if(menu == 4) {
+			nList.add("T");
+			nList.add("G");
+		} else if(menu == 5) {
+			nList.add("T");
+			nList.add("A");
+		} else if(menu == 6) {
+			nList.add("G");
+			nList.add("A");
+		} else if(menu == 7) {
+			nList.add("T");
+			nList.add("G");
+			nList.add("A");
+		}
+		
+		Object[] fsArr = nList.toArray();
+		System.out.print("검색어 입력 : ");
+		String ss = scan.next();
+		
+		map.put("fsArr", fsArr);
+		map.put("ss", ss);
+		
+		list = dao.movieFindData(map);
+		
+		for(MovieVO vo : list) {
+			System.out.println(vo.getTitle());
+			System.out.println(vo.getGenre());
+			System.out.println(vo.getActor());
+			System.out.println("=======================");
+		}
+	}
+}
