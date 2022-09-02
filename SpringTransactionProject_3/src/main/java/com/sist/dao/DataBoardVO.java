07248d1ecbca38c7a54d4aee5c9a78dddf5bc65b
ajskd5 <ajskd5@naver.com>
2022-09-02 16:55:28 +0900

    멀티 파일 업로드 + 자동로그인

---
+++ SpringTransactionProject_3/src/main/java/com/sist/dao/DataBoardVO.java
@@ -0,0 +1,28 @@
+package com.sist.dao;
+/*
+NO        NOT NULL NUMBER         
+NAME      NOT NULL VARCHAR2(34)   
+SUBJECT   NOT NULL VARCHAR2(1000) 
+CONTENT   NOT NULL CLOB           
+PWD       NOT NULL VARCHAR2(10)   
+REGDATE            DATE           
+HIT                NUMBER         
+FILENAME           VARCHAR2(2000) 
+FILESIZE           VARCHAR2(1000) 
+FILECOUNT          NUMBER   
+ */
+import java.util.*;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class DataBoardVO {
+	private int no, hit, filecount;
+	private String name, subject, content, pwd, filename, filesize;
+	private Date regdate;
+	private List<MultipartFile> files; // 업로드된 파일 받을 때 사용 (여러개 => List)
+	
+}
---
+++ SpringTransactionProject_3/src/main/java/com/sist/intercepter/LoginIntercepter.java
@@ -0,0 +1,5 @@
+package com.sist.intercepter;
+// 자동 로그인 (세션)
+public class LoginIntercepter {
+	
+}
